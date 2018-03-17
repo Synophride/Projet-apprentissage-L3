bb8324c5b73b6e1d2824b74d718056bd6c0de9c1
John Doe <synophride@EL.MONO>
2018-03-17 14:29:02 +0100

    Plateau 17/03

--- src/main/java/jeux/quarto/PlateauQuarto.java
+++ src/main/java/jeux/quarto/PlateauQuarto.java
@@ -8,20 +8,42 @@ import java.util.ArrayList;
 
 
 public class PlateauQuarto implements PlateauJeu, Partie1 {
-    private long plateau;
-    private short indCases;
-    private short indPiece;
-    private byte tourEtPiece;
+    private unsigned long plateau;
+    private unsigned short indCases;
+    private unsigned short indPiece;
+    private unsigned byte tourEtPiece;
 
+    /* AUCUN TEST. On part du principe que le coup est valide */
+    private void unsafe_jouerCoup(byte coup){
+	byte piece = (coup & 0x0F), coord = coup >> 4;
+	plateau = plateau | (piece << (4*coord));
+    }
+
+    
     public void setFromFile(String fileName){
 	
     }
     
     public void saveToFile(String fileName){
+	
     }
 
     public boolean estmoveValide(String move, String player){
+	
     }
+
+    public String[] mouvementsPossibles(String player){
+
+    }
+
+    public void play(String move, String player){
+
+    }
+
+    public boolean finDePartie(){
+
+    }
+
 }
 
 
